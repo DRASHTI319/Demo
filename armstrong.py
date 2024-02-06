c5e917fd6a777f27a300beb38a7edc56158f0bb2
DRASHTI319 <130157722+DRASHTI319@users.noreply.github.com>
2024-02-06 10:47:51 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num=int(input('Enter a number'))
+sum=0
+Temp=num
+while Temp>0:
+  digit=Temp%10
+  sum+=digit**3
+  temp
+
+if num == sum
+  print (num,'is an armstrong number')
+else
+  print (num, 'is not armstrong number')
